7a7be17d354fdd36cbda723161f6856f084f9dcf
Mark Liffiton <liffiton@gmail.com>
2013-06-20 13:46:13 -0500

    Refactoring.  No more duplicate code.  (Except a bit in the tests...)

--- minisolvers.py
+++ minisolvers.py
@@ -1,20 +1,21 @@
+import array
 import os
 import ctypes
 from ctypes import c_void_p, c_ubyte, c_bool, c_int
 
 class Solver(object):
-    """The Solver class is meant as a fairly direct analog of MiniCard's Solver class."""
+    """The Solver class is meant as a fairly direct analog of Minisat/Minicard's Solver class."""
 
-    def __init__(self):
-        """Load the minicard library with ctypes and create a Solver object."""
+    def __init__(self, libfilename):
+        """Load the minisat library with ctypes and create a Solver object."""
         dirname = os.path.dirname(os.path.abspath(__file__))
-        self.lib = ctypes.cdll.LoadLibrary(dirname+'/libminicard.so')
+        self.lib = ctypes.cdll.LoadLibrary(dirname+'/'+libfilename)
         self._setup_lib()
         self.s = self.lib.Solver_new()
 
     def _setup_lib(self):
         """Correct return types (if not int as assumed by ctypes) and set argtypes for
-           functions from the minicard library.
+           functions from the minisat library.
         """
         l = self.lib
 
@@ -27,8 +28,6 @@ class Solver(object):
 
         l.newVar.argtypes = [c_void_p, c_ubyte]
 
-        l.addAtMost.restype = c_bool
-        l.addAtMost.argtypes = [c_void_p, c_int, c_void_p, c_int]
         l.addClause.restype = c_bool
         l.addClause.argtypes = [c_void_p, c_int, c_void_p]
         l.addUnit.restype = c_bool
@@ -53,13 +52,18 @@ class Solver(object):
         """Delete the Solver object"""
         self.lib.Solver_delete(self.s)
 
+    @staticmethod
+    def _to_intptr(a):
+        """Helper function to get a ctypes POINTER(c_int) for an array"""
+        return ctypes.cast(a.buffer_info()[0], ctypes.POINTER(c_int))
+
     def new_var(self, polarity=None):
         """Create a new variable in the solver.
         
         Args:
             polarity: A boolean specifying the default polarity for this
                 variable.  True = variable's default is True, etc.  Note
-                that this is the reverse of the 'user polarity' in minicard,
+                that this is the reverse of the 'user polarity' in minisat,
                 where True indicates the *sign* is True, hence the default
                 value is False.
 
@@ -81,27 +85,6 @@ class Solver(object):
     def nclauses(self):
         return self.lib.nClauses(self.s)
 
-    def add_atmost(self, lits, k):
-        """Add an AtMost constraint to the solver.
-
-        Args:
-            lits: A list of literals as integers.  Each integer specifies a
-                variable with *1*-based counting and a sign via the sign of
-                the integer.  Ex.: [-1, 2, -3] is [!x0, x1, !x2]
-            k: The [upper] bound to place on these literals
-
-        Returns:
-            A boolean value returned from MiniCard's addAtMost() function,
-            indicating success (True) or conflict (False).
-        """
-        if not all([abs(x) <= self.nvars() for x in lits]):
-            raise Exception("Not all variables in %s are created yet.  Call new_var() first." % lits)
-        if len(lits) > 1:
-            array = (c_int * len(lits))(*lits)
-            self.lib.addAtMost(self.s, len(lits), array, k)
-        else:
-            self.lib.addAtMost(self.s, 0, None, 0)
-
     def add_clause(self, lits):
         """Add a clause to the solver.
 
@@ -111,14 +94,15 @@ class Solver(object):
                 the integer.  Ex.: [-1, 2, -3] is [!x0, x1, !x2]
 
         Returns:
-            A boolean value returned from MiniCard's addClause() function,
+            A boolean value returned from Minisat's addClause() function,
             indicating success (True) or conflict (False).
         """
-        if not all([abs(x) <= self.nvars() for x in lits]):
+        if not all(abs(x) <= self.nvars() for x in lits):
             raise Exception("Not all variables in %s are created yet.  Call new_var() first." % lits)
         if len(lits) > 1:
-            array = (c_int * len(lits))(*lits)
-            self.lib.addClause(self.s, len(lits), array)
+            a = array.array('i',lits)
+            a_ptr = self._to_intptr(a)
+            self.lib.addClause(self.s, len(lits), a_ptr)
         elif len(lits) == 1:
             self.lib.addUnit(self.s, lits[0])
         else:
@@ -136,8 +120,9 @@ class Solver(object):
         if assumptions is None:
             return self.lib.solve(self.s)
         else:
-            array = (c_int * len(assumptions))(*assumptions)
-            return self.lib.solve_assumptions(self.s, len(assumptions), array)
+            a = array.array('i',assumptions)
+            a_ptr = self._to_intptr(a)
+            return self.lib.solve_assumptions(self.s, len(assumptions), a_ptr)
 
     def simplify(self):
         return self.lib.simplify(self.s)
@@ -155,9 +140,9 @@ class Solver(object):
         """
         if end == -1:
             end = self.nvars()
-        array = (c_int * (end-start))()
-        self.lib.fillModel(self.s, array, start, end)
-        return array[:]
+        a = (c_int * (end-start))()
+        self.lib.fillModel(self.s, a, start, end)
+        return a[:]
 
     def get_model_trues(self, start=0, end=-1):
         """Get variables assigned true in the current model from the solver.
@@ -171,24 +156,18 @@ class Solver(object):
         """
         if end == -1:
             end = self.nvars()
-        array = (c_int * (end-start))()
-        count = self.lib.getModelTrues(self.s, array, start, end)
-        return array[:count]
+        a = (c_int * (end-start))()
+        count = self.lib.getModelTrues(self.s, a, start, end)
+        return a[:count]
 
     def model_value(self, i):
         return self.lib.modelValue(self.s, i)
 
-class SubsetSolver(Solver):
-    """A subclass of the Solver class specifically for reasoning about subsets of a clause set.
-
-    TODO: additional documentation, example.
-    """
-
-    def __init__(self):
-        self.origvars = None
-        self.origclauses = None
-        self.n = 0
-        super(SubsetSolver, self).__init__()
+class SubsetMixin(object):
+    """A mixin for any Solver class that lets it reason about subsets of a clause set."""
+    origvars = None
+    origclauses = None
+    n = 0
 
     def set_orig(self, o_vars, o_clauses):
         """Record how many of the solver's variables and clauses are "original,"
@@ -201,19 +180,65 @@ class SubsetSolver(Solver):
         if self.origvars is None:
             raise Exception("SubsetSolver.set_orig() must be called before .add_clause()")
         instrumented_clause = [-(self.origvars+self.n+1)] + lits
-        super(SubsetSolver, self).add_clause(instrumented_clause)
+        super(SubsetMixin, self).add_clause(instrumented_clause)
         self.n += 1
 
     def solve_subset(self, subset):
-        array = (c_int * len(subset))(*subset)
-        return self.lib.solve_subset(self.s, self.origvars, len(subset), array)
+        a = array.array('i', subset)
+        a_ptr = self._to_intptr(a)
+        return self.lib.solve_subset(self.s, self.origvars, len(subset), a_ptr)
 
     def unsat_core(self):
-        array = (c_int * self.nclauses())()
-        length = self.lib.unsatCore(self.s, self.origvars, array)
-        return array[:length]
+        a = (c_int * self.nclauses())()
+        length = self.lib.unsatCore(self.s, self.origvars, a)
+        return a[:length]
 
     def sat_subset(self):
-        model = self.get_model(start = self.origvars, end = self.origvars+self.origclauses)
-        return [i for i in range(self.n) if model[i]]
+        return self.get_model_trues(start = self.origvars, end = self.origvars+self.origclauses)
+
+class MinisatSolver(Solver):
+    def __init__(self):
+        super(MinisatSolver, self).__init__("libminisat.so")
+
+class MinicardSolver(Solver):
+    def __init__(self):
+        super(MinicardSolver, self).__init__("libminicard.so")
+
+    def _setup_lib(self):
+        """Correct return types (if not int as assumed by ctypes) and set argtypes for
+           functions from the minicard library.
+        """
+        super(MinicardSolver, self)._setup_lib()
+
+        # additional function for minicard
+        l = self.lib
+        l.addAtMost.restype = c_bool
+        l.addAtMost.argtypes = [c_void_p, c_int, c_void_p, c_int]
+
+    def add_atmost(self, lits, k):
+        """Add an AtMost constraint to the solver.
+
+        Args:
+            lits: A list of literals as integers.  Each integer specifies a
+                variable with *1*-based counting and a sign via the sign of
+                the integer.  Ex.: [-1, 2, -3] is [!x0, x1, !x2]
+            k: The [upper] bound to place on these literals
+
+        Returns:
+            A boolean value returned from MiniCard's addAtMost() function,
+            indicating success (True) or conflict (False).
+        """
+        if not all(abs(x) <= self.nvars() for x in lits):
+            raise Exception("Not all variables in %s are created yet.  Call new_var() first." % lits)
+        if len(lits) > 1:
+            array = (c_int * len(lits))(*lits)
+            self.lib.addAtMost(self.s, len(lits), array, k)
+        else:
+            self.lib.addAtMost(self.s, 0, None, 0)
+
+class MinisatSubsetSolver(SubsetMixin, MinisatSolver):
+    pass
+
+class MinicardSubsetSolver(SubsetMixin, MinicardSolver):
+    pass
 
--- test_minisolvers.py
+++ test_minisolvers.py
@@ -1,10 +1,57 @@
-
-import minicard
+import minisolvers
 import unittest
+
+class MinisatTest(unittest.TestCase):
+    def setUp(self):
+        self.solver = minisolvers.MinisatSolver()
+        self.clauses = [ [1], [-2], [3, 4], [-3, 5], [-4, 6], [-5, 4], [-6] ]
+        self.numvars = max([max(cl) for cl in [[abs(x) for x in cl] for cl in self.clauses]])
+
+    def tearDown(self):
+        del self.solver
+
+    def test_newvars(self):
+        for i in range(self.numvars):
+            self.solver.new_var(True)
+            self.assertEqual(self.solver.nvars(), i+1)
+
+    def test_add_clause_without_vars(self):
+        self.assertRaises(Exception, self.solver.add_clause, [-1,2])
+
+    def add_subset(self, subset):
+        for i in range(self.numvars):
+            self.solver.new_var()
+        for cl in subset:
+            self.solver.add_clause(cl)
+
+    def test_sat(self):
+        self.add_subset(self.clauses[:-1])
+        self.assertEqual(self.solver.solve(), True)
+
+    def test_unsat(self):
+        self.add_subset(self.clauses)
+        self.assertEqual(self.solver.solve(), False)
+
+    def test_assumptions(self):
+        self.add_subset(self.clauses[:-2])
+        self.assertEqual(self.solver.solve([-5]), True)
+        self.assertEqual(self.solver.solve([-6]), True)
+        self.assertEqual(self.solver.solve([-5,-6]), False)
+
+    def test_model(self):
+        from math import copysign
+        isPositive = lambda x: copysign(1,x) > 0
+        subset = self.clauses[:-1]
+        self.add_subset(subset)
+        self.solver.solve()
+        m = self.solver.get_model()
+        self.assertEqual(len(m), self.solver.nvars())
+        for cl in subset:
+            self.assertTrue(any([ m[abs(x)-1] == isPositive(x) for x in cl ]))
 
 class MinicardTest(unittest.TestCase):
     def setUp(self):
-        self.solver = minicard.Solver()
+        self.solver = minisolvers.MinicardSolver()
         self.atmosts = [(list(range(1,31)), 15)]
         self.clauses = [ [15], [10], [8], [12, 5], [4, 6], [2], [1], [-1, -2, -6, -8, -10, -12, -15] ]
         self.assumptions = [-x for x in range(16,31)]
