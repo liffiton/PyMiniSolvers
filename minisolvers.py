5d147e74b33de174d8c12e073aa54da545248123
Wenting Zhao <wentingzhao@outlook.com>
2017-04-01 21:43:25 -0500

    Final fix for the block_model() method. Well-tested.

--- minisolvers.py
+++ minisolvers.py
@@ -296,7 +296,8 @@ class Solver(object):
 
     def block_model(self):
         """Block the current model from the solver."""
-        self.add_clause([-i for i in self.get_model_trues()])
+        model = self.get_model()
+        self.add_clause([-(x+1) if model[x] > 0 else x+1 for x in range(1, len(model))])
 
     def model_value(self, i):  # type: (int) -> bool
         '''Get the value of a given variable in the current model.'''
